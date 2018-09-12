e965d503226eb32d8e9e31fca9f2449d589d6ef5
Renan Beux <nanerxueb@hotmail.com>
2018-09-12 16:05:35 -0300

    12/09/18 1605

---
+++ App12_WebService/App12_WebService/Service/ServiceWS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using App12_WebService.Model;
+
+namespace App12_WebService.Service
+{
+    public class ServiceWS
+    {
+        private static string Enderecobase = "http://ws.spacedu.com.br/xf2018/rest/api";
+
+        public static Usuario GetUsuario(Usuario usuario)
+        {
+            HttpClient requisicao = new HttpClient();
+            HttpResponseMessage resposta = requisicao.PostAsync("Url", "parametros").GetAwaiter().GetResult();
+
+            if(resposta.StatusCode == HttpStatusCode.OK)
+            {
+                //TODO - Deserealizar, retornar no met e salvar como login.
+            }
+
+            return null;
+        }
+    }
+}
